9d1de4794a3e69128e9e40a4668d54b0862d30cf
Mark Tillinghast <complexity.19.mark314159@spamgourmet.com>
2017-04-19 14:20:48 -0700

    modified:   cellularAutoma.py

--- cellularAutoma.py
+++ cellularAutoma.py
@@ -4,8 +4,8 @@
 # of python for use in any way for the betterment of humankind.
 # (c) 2017
 rule_110="01101110"
-
-dict_110 = {"000" : "0",
+rule_102="01100110"
+dict_102 = {"000" : "0",
             "001" : "1", 
             "010" : "1",
             "011" : "0",
@@ -23,6 +23,6 @@ for itheir in  range(0,900):
     next_line=""
     for j in range(0,len(current_line)-3): #why does this fail badly when =1
        #print itheir,j,current_line[j:j+3],dict_110[current_line[j:j+3]]
-       next_line=next_line+dict_110[current_line[j:j+3]]
+       next_line=next_line+dict_102[current_line[j:j+3]]
     print next_line
     current_line="0"+next_line+"00" 
